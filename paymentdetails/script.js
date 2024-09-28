37873f107e6b1dc0cb19345c6bc154e9b71f5b23
Nwankwo Emmanuel <emmatexiii@gmail.com>
2024-09-28 02:16:51 +0100

    paymentpage update

--- paymentdetails/script.js
+++ paymentdetails/script.js
@@ -3,6 +3,24 @@ document.querySelector('.subscribe-btn').addEventListener('click', function() {
     alert('Subscription confirmed!');
 });
 
-document.querySelector('.cancel-btn').addEventListener('click', function() {
-    alert('Subscription canceled!');
+$(document).ready(function() {
+    let currentPlanIndex = 1;
+    const totalPlans = 3;
+
+    // Function to switch between plans
+    function switchPlan() {
+        // Hide current plan
+        $(`#plan-${currentPlanIndex}`).hide();
+
+        // Increment the index, and wrap back to 1 if we exceed total plans
+        currentPlanIndex = (currentPlanIndex % totalPlans) + 1;
+
+        // Show the next plan
+        $(`#plan-${currentPlanIndex}`).show();
+    }
+
+    // On clicking the change plan button, switch plans
+    $(".change-plan-btn").click(function() {
+        switchPlan();
+    });
 });
